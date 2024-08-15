c133a323fcdbb7b96d38b96f0663915e7c97607d
Ashwin-V-17 <ashwinv1707@gmail.com>
2024-08-15 15:41:52 +0530

    login logout functionalties in the frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,6 +4,9 @@ export const StoreContext=createContext(null);
 
 const StoreContextProvider=(props)=>{
     const [cartItems,setCartItems]=useState({});
+    const url="http://localhost:4000";
+
+    const [token,setToken]=useState("");
     const addToCart=(itemId)=>{
         if(!cartItems[itemId])//First entry of the item
         {
@@ -29,13 +32,22 @@ const StoreContextProvider=(props)=>{
         }
         return totalAmount;
     }
+    useEffect(()=>{
+        if(localStorage.getItem("token"))
+        {
+           setToken(localStorage.getItem("token"));//It helps to the user to keep logged in even if the refresh button is clicked
+        }
+    },[])
     const contextValue={
        food_list,
        cartItems,
        setCartItems,
        addToCart,
        removeFromCart,
-       getTotalCartAmount
+       getTotalCartAmount,
+       url,
+       token,
+       setToken
     }
     return(
         <StoreContext.Provider value={contextValue}>
